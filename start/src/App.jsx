11f3ca6d9ece729cc9344db39ff2e596d3a9cd5e
9IcTpy6 <9i.c.tpy6@gmail.com>
2021-11-23 22:27:25 +0200

    Initial commit

---
+++ start/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <ToDolist />
+    );
+};
+
+export default App;
---
+++ start/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App.jsx';
+
+ReactDOM.render(
+    <h1 class="title">To DO List</h1>,
+    document.getElementById('root')
+);
